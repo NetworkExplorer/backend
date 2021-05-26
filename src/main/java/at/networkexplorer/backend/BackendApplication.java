cca8ea571ce6786a17079f3d11cf1a9f664eafdf
0Adiber <adiber.yt@gmail.com>
2021-05-26 11:59:20 +0200

    :card_file_box: Removed DB for now

--- src/main/java/at/networkexplorer/backend/BackendApplication.java
+++ src/main/java/at/networkexplorer/backend/BackendApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BackendApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
